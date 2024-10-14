3cfdf49638e7cf0559daa38be2c00b649d6967b9
brun-brito <brunocaudebrito@hotmail.com>
2024-10-14 10:28:44 -0300

    colocando status 403 para pagamento nao aprovado

--- src/controllers/profissionalController.js
+++ src/controllers/profissionalController.js
@@ -21,9 +21,17 @@ async function verificarCadastro(req, res) {
             });
         }
 
-        // Se o usuário foi encontrado e o CEP retornado, envia a resposta
+        // Verifica se o campo "pagamento" é true
+        if (!profissional.pagamento) {
+            return res.status(403).json({ 
+                message: 'Usuário encontrado, mas o pagamento não está em dia.', 
+                status: false 
+            });
+        }
+
+        // Se o usuário foi encontrado e o pagamento está em dia, envia a resposta
         return res.status(200).json({ 
-            message: 'Usuário está cadastrado.', 
+            message: 'Usuário está cadastrado e o pagamento está em dia.', 
             status: true, 
             cep: profissional.cep,
             nome: profissional.nome,
